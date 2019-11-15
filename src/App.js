d5951fe9e33ae3d380ba5132e7d6566609524807
Sam <super8989@gmail.com>
2019-11-14 22:15:42 -0500

    example of Routes but this is not practical

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import Food from "./Food";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<Food name='Kale' />
+				<Route path='/food/kale' render={() => <Food name='kale' />} />
+				<Route path='/food/chicken' render={() => <Food name='chicken' />} />
+				<Route
+					path='/food/pineapple'
+					render={() => <Food name='pineapple' />}
+				/>
 			</div>
 		);
 	}
