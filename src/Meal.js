3e76d5313affa98ebec160b6b84260c5a805e14f
Sam <super8989@gmail.com>
2019-11-16 21:47:22 -0500

    multiple route params demo with Meal component

--- src/Meal.js
+++ src/Meal.js
@@ -2,9 +2,18 @@ import React, { Component } from "react";
 
 class Meal extends Component {
 	render() {
+		const foodName = this.props.match.params.foodName;
+		const drinkName = this.props.match.params.drinkName;
+		const foodUrl = `https://source.unsplash.com/1600x900/?${foodName}`;
+		const drinkUrl = `https://source.unsplash.com/1600x900/?${drinkName}`;
+
 		return (
 			<div>
-				<h1>This is a Meal</h1>
+				<h1>
+					This is a Meal of {foodName} + {drinkName}{" "}
+				</h1>
+				<img src={foodUrl} />
+				<img src={drinkUrl} />
 			</div>
 		);
 	}
