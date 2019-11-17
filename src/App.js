bb0966c6a4d0957a77b215371ab79d4ccdc98b04
Sam <super8989@gmail.com>
2019-11-16 21:52:06 -0500

    adding a 404 not found route using switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
 import Food from "./Food";
 import Meal from "./Meal";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				{/* <Route path='/food/:name' component={Food} /> */}
-				<Route
-					exact
-					path='/food/:name'
-					render={routeProps => <Food {...routeProps} />}
-				/>
-				<Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
+				<Switch>
+					{/* <Route path='/food/:name' component={Food} /> */}
+					<Route
+						exact
+						path='/food/:name'
+						render={routeProps => <Food {...routeProps} />}
+					/>
+					<Route
+						exact
+						path='/food/:foodName/drink/:drinkName'
+						component={Meal}
+					/>
+					<Route exact path='/' render={() => <h1>Home Page</h1>} />
+					<Route render={() => <h1>Error Not Found</h1>} />
+				</Switch>
 			</div>
 		);
 	}
