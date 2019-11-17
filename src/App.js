d969bc1251141d2b3f831aa0026552fe97a7816f
Sam <super8989@gmail.com>
2019-11-16 21:41:19 -0500

    added in Meal component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Food from "./Food";
+import Meal from "./Meal";
 import { Route } from "react-router-dom";
 import "./App.css";
 
@@ -7,11 +8,13 @@ class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<Route
-					path='/food/:name'
-					render={routeProps => <Food authenticated={true} {...routeProps} />}
-				/>
 				{/* <Route path='/food/:name' component={Food} /> */}
+				<Route
+					exact
+					path='/food/:name'
+					render={routeProps => <Food {...routeProps} />}
+				/>
+				<Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
 			</div>
 		);
 	}
